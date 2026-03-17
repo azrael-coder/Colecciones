e442a33adff24618400126b71c2cd502c05fe751
azrael <israelgonzalez050908@gmail.com>
2026-03-17 12:33:13 +0100

    terminar

--- src/ejercicios/Ejercicio1.java
+++ src/ejercicios/Ejercicio1.java
@@ -1,57 +1,84 @@
 package ejercicios;
+/*
+ * Realiza un programa que introduzca valores aleatorios (entre 0 y 100) en un
+ * ArrayList y que luego realice las siguientes operaciones: mostrar el contenido,
+ * calcular la suma, borrar aquellos números que serán pares y volver a mostrar el
+ * contenido, remplazar los números divisibles por 3 con el valor 0 y volver a
+ * mostrar el contenido, por último, si aparece un valor divisible entre cinco,
+ * insertar en la posición anterior el valor 555 y volver a mostrar el contenido. El
+ * tamaño de la lista también será aleatorio y podrá oscilar entre 10 y 20 elementos
+ * ambos inclusive
+ */
 
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Random;
 
 public class Ejercicio1 {
+    public static ArrayList<Integer> eliminarPares(ArrayList<Integer> pares){
+
+        for (int i = 0; i < pares.size(); i++) {
+
+            if (pares.get(i) % 2 == 0){
+                pares.remove(i);
+            }
+        }
+
+        return  pares;
+    }
+
     public static void main(String[] args) {
         Random r = new Random();
-        ArrayList<Integer> lista = new ArrayList<>();
         int num;
         int suma = 0;
         int tamanio = r.nextInt(20 - 10 + 1) + 10;
+        ArrayList<Integer> lista = new ArrayList<>(tamanio);
 
-        System.out.println("---------CONTENIDO---------");
-        for (int i = 0; i < tamanio; i++) {
+        System.out.println("---------CONTENIDO NORMAL---------");
+        int contador = 1;
+        for (int i = 0; i < tamanio; i++) { //LLENAR ArrayList
+
             lista.add(r.nextInt(101));
             num = lista.get(i);
-            System.out.println(num);
+            System.out.println("Numero " + contador +  ":" + num);
             suma += num;
+
+            contador++;
         }
 
+        System.out.println();
         System.out.println("---------SUMA---------");
         System.out.println(suma);
 
-        /**
-         * ---------ELIMINAR Y REEMPLAZAR PARES Y DIVISIBLES POR 3---------
-         */
+        lista = eliminarPares(lista); //ELIMINAR PARES (en progreso)
+
+         //---------REEMPLAZAR  DIVISIBLES POR 3---------
+
         for (int i = 0; i < lista.size(); i++) {
 
-            if (lista.get(i) % 2 == 0){
-                lista.remove(i);
-            }
-            else if (lista.get(i) % 3 == 0){
+            if (lista.get(i) % 3 == 0){
                 lista.set(i,0);
             }
-
         }
 
-        /**
-         * EXEPCIO IndexOutOfBounds
-         */
+        System.out.println("---------PRUEBA---------");
         for (Integer i : lista) {
-            if (i % 5 == 0)
+            System.out.print(i + " - ");
+        }
+
+        for (int i = 0; i<lista.size();i++) {
+            if (lista.get(i) % 5 == 0)
                 lista.set(i - 1, 555);
         }
 
+        System.out.println();
 
         System.out.println("---------LISTA FINALIZADA---------");
         Iterator<Integer> iterator = lista.iterator();
         while (iterator.hasNext()){
             int numero;
             numero = iterator.next();
-            System.out.println(numero);
+            System.out.print(numero+ "-");
         }
 
     }
