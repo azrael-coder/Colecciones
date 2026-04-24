8f2e05d8447db095aace28cd3cd24ce55785b3b9
azrael <israelgonzalez050908@gmail.com>
2026-04-24 10:42:16 +0200

    done

--- src/ejercicio7/Ejercicio7.java
+++ src/ejercicio7/Ejercicio7.java
@@ -17,20 +17,23 @@ public class Ejercicio7 {
         diccionario.put("caminando", "walking");
         diccionario.put("conducir", "drive");
 
+
+        //Pasar el diccionario a ArrayList
         ArrayList<String> lista = new ArrayList<>(diccionario.keySet());
         int contador = 0;
         int correctas = 0;
         int incorrectos = 0;
 
-        Collections.shuffle(lista);
+        Collections.shuffle(lista); //El metodo shuffle desordena la lista de forma aleatoria
         Scanner leer = new Scanner(System.in);
         do{
 
             String palabra = lista.get(contador);
             System.out.print("Ingrese la traduccion en ingles de: " + palabra + ": ");
-            String traduccion = leer.nextLine().toLowerCase().trim();
+            String traduccion = leer.nextLine().toLowerCase().trim(); //quitar espacios en blanco y pasarla a minuscula
 
-            String respuestaCorrecta = diccionario.get(palabra); // Pasandole la clave introducida por el usuario busca la palabra traduccion correcta
+            String respuestaCorrecta = diccionario.get(palabra); // Pasandole la palabra extraida de la lista en español con indice 'contador'
+            // al get() de diccionario extraigo la respuesta correcta luego comparo
 
             if (traduccion.equals(respuestaCorrecta)) {
                 System.out.println("Correcta!!");
@@ -48,7 +51,7 @@ public class Ejercicio7 {
         System.out.println("Correctos: " + correctas + " incorrectos: " + incorrectos);
 
         System.out.println("=====Ordenadas=====");
-        lista.sort((a,b)-> b.length() - a.length());
+        lista.sort((a,b)-> a.length() - b.length());
         for (String p : lista) {
             System.out.println(p + " | " + diccionario.get(p) );
         }
